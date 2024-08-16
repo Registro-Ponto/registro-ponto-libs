7ccd885e5d63c663edc2dfa9a53b3cf3b8ff90c4
Kenji <98567681+kenji-webdev@users.noreply.github.com>
2024-08-16 16:04:32 -0300

    feat: configura ci de publicação no npm (#6)
    - Adiciona workflows para publicação no npm (está faltando o secret do
    npm)
    - Adiciona CHANGELOG.md de ícones e ilustrações
    - Altera `release-notes.js` e `release-channel.js` para aceitarem o nome
    da pasta do pacote pelo parâmetro no `package.json`
    - Foi criado apenas um arquivo `release.yaml` porque pelo que vi não dá
    pra separar com as tags como estamos fazendo agora para o
    `icons-prepare-release` e `illustrations-prepare-release`
    - Ele é executado ao gerar um novo release com uma tag que siga o
    formato `illustrations-v*.*.*` ou `icons-v*.*.*` e retorna um erro caso
    essa tag não siga esses formatos

--- scripts/release-notes.js
+++ scripts/release-notes.js
@@ -4,10 +4,40 @@
 let path = require('path')
 let fs = require('fs')
 
-let version =
-  process.argv[2] || process.env.npm_package_version || require('../package.json').version
+/**
+ * Normalize newlines in the changelog string, fixes issue when running on Windows.
+ * Doesn't do anything in CI but I think it's good to have it here so we don't waste time again in the future.
+ */
+function normalizeNewlines(text) {
+  return text.replace(/\r\n/g, '\n').replace(/\r/g, '\n')
+}
 
-let changelog = fs.readFileSync(path.resolve(__dirname, '..', 'CHANGELOG.md'), 'utf8')
+/**
+ * @typedef {"rp-icons" | "rp-illustrations"} PackageType
+ */
+
+/**
+ * @see {@link PackageType}
+ */
+const packageType = process.argv[2]
+
+if (!['rp-icons', 'rp-illustrations'].includes(packageType)) {
+  throw new Error('Invalid package type. Must be "rp-icons" or "rp-illustrations".')
+}
+
+let version = process.env.npm_package_version || require(`../${packageType}/package.json`).version
+
+/**
+ * Reads the CHANGELOG.md file for a given {@link PackageType} and extracts the changelog entry for a specified version.
+ * @description The changelog for the current package version.
+ */
+const changelog = normalizeNewlines(
+  fs.readFileSync(path.resolve(__dirname, '..', packageType, 'CHANGELOG.md'), 'utf8')
+)
+
+/**
+ * Whether the version is in the changelog for the current package @see {@link PackageType}
+ */
 let match = new RegExp(
   `## \\[${version}\\] - (.*)\\n\\n([\\s\\S]*?)\\n(?:(?:##\\s)|(?:\\[))`,
   'g'
